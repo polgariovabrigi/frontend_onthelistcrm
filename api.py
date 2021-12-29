135e572e6645a3e57e30264cfb1a01bf5c8b8398
brigita polgariova <brigita@pobtrading.com>
2021-12-29 15:17:38 +0100

    push code

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 # import pickle 
 # import pandas as pd
@@ -11,13 +11,27 @@ app = FastAPI()
 def root():
     return "Hello from Cloud Run CD"
 
-#segmentation route
-#get the uploaded files
+# #segmentation route
+# #get the uploaded files
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile = File(...)):
+#    return {
+#       "filename": file.filename
+#    }
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
    return {
-      "filename": file.filename
+      "filename" : file.filename
    }
+
+
+# UploadFileapp = FastAPI()
+# @app.post("/image") 
+# async def image(image: UploadFile = File(...)): 
+#     return {
+#    "filename": image.filename
+# }
 
 # @app.post("/get_csv", response_class = StreamingResponse)
 # def get_csv(file: bytes = File(...)):

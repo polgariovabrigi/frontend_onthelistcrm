c526689c3c34d344c9cd5e976e02f434c42606a9
brigita polgariova <brigita@pobtrading.com>
2021-12-29 14:02:18 +0100

    api response/call

--- api.py
+++ api.py
@@ -1,15 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
+from fastapi.responses import StreamingResponse
+import pickle 
+import pandas as pd
+import io
+from io import StringIO
 
 app = FastAPI()
 
+#main page to select what type of prediction client wishes for
 @app.get("/")
 def root():
     return "On The List AI Prediction"
 
 #segmentation route
-@app.get("/predict_segment")
-def predict_segment(csv):
-    return "Testing our deployment"
+#get the uploaded files
+@app.post("/get_csv", response_class = StreamingResponse)
+async def get_csv(file: bytes = File(...)):
+    # file as str
+    inputFileAsStr = io.StringIO(str(file,'utf-8'))
+    # dataframe
+    df = pd.read_csv(inputFileAsStr)
+    # output file
+    outFileAsStr = StringIO()
+    df.to_csv(outFileAsStr, index = False)
+    response = StreamingResponse(io.StringIO(df.to_csv(index=False), media_type="csv"),
+        headers={
+            'Content-Disposition': 'attachment;filename=dataset.csv',
+            'Access-Control-Expose-Headers': 'Content-Disposition'
+        }
+    )
+    return response
+
+#run the model 
+# @app.get("/predict_segment")
+# def predict_segment(csv):
+#     model = pickle.load(open('kmean_model_28_12_2021_16h_44.sav', 'rb'))
+#     prediction = model.predict(data)
+#     return prediction 
+
 
 #customerß
 @app.get("/predict_product")
--- app.py
+++ app.py
@@ -1,5 +1,35 @@
+import streamlit as st
 import requests
+import pandas as pd
     
-url = 'http://localhost:8000/predict'
+# url = 'http://localhost:8000/predict'
+# response = requests.get(url)
+# response.json()
+
+'''
+# OnTheList Segmentation Model
+
+Model returns segments for each client in CSV file
+'''
+
+'''
+Please upload CSV file
+'''
+#upload file
+uploaded_file = st.file_uploader(
+    "Upload your csv file", type=["csv"], accept_multiple_files=False)
+
+#read file
+#try/except
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  # st.write(df)
+  
+  
+# enter here the address of your flask api
+url = '/'
 response = requests.get(url)
-response.json()
+prediction = response.json()
+# pred = prediction['prediction']
+# pred
+

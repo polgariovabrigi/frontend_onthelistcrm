3a288e2389df1005771467a91099a181af2dac7b
benjamin barre <benjamin.barre@gmail.com>
2022-01-08 18:08:35 +0800

    api and app update

--- api.py
+++ api.py
@@ -1,42 +1,45 @@
 from fastapi import FastAPI, File, UploadFile
 import pandas as pd
-
-from on_the_list_crm.cleansing_dataset import BasicCleaner
-from on_the_list_crm.vendor_cat import vendor_cat
-from on_the_list_crm.product_cat_and_gender import load_nlp_model , load_tokenizer, transform_dataset
 from on_the_list_crm.onthelist_segmentation import Segmentation
-
-# from on_the_list_crm.onthelist_segmentation import Segmentation
-# from on_the_list_crm.product_cat_and_gender import transform_dataset
+from on_the_list_crm.product_cat_and_gender import transform_dataset
+from io import StringIO
 
 #main page for testing API
 app = FastAPI()
 @app.get("/")
 def root():
-    return "Hello from Cloud Run CD"
+    return "Hello from Testing"
 
 # #segmentation route
 @app.post("/uploadfile/")
-async def upload_file(csv_file: UploadFile = File(...)):
-    contents = await csv_file.read()
+async def create_upload_file(file: UploadFile = File(...)):
+    content = await file.read()
+    # df = pd.read_csv(StringIO(str(content)), encoding='utf-8')
+    # print(type(df))
+    print(type(content))
+    return content
 
-    df = pd.read_csv(csv_file.file)
+# #segmentation route
+# @app.post("/uploadfile")
+# async def upload_file(file: UploadFile = File(...)):
+#     contents = await file.file
+#     return {"filename": contents}
+    # df = pd.read_csv(csv_file.file)
 
     # data_df = pd.read_dict(contents)
     # data_df = transform_dataset(data_df)
     # segmentation = Segmentation(data_df)
     # segmentation.load_km_model()
     # segment_df = segmentation.predict()
-    return df
 
 
     #return prediction
     # return {"filename": csv_file.filename,
     #         "filetype": csv_file.content_type,
 
-    #         "content": contents 
-    #         }  
-    
+    #         "content": contents
+    #         }
+
 #Loading the trained model
 # with open("./finalized_model.pkl", "rb") as f:
 #     loaded_model = pickle.load(f)
@@ -46,4 +49,3 @@ async def upload_file(csv_file: UploadFile = File(...)):
 # @app.get("/predict_product")
 # def predict_customer():
 #     return "Testing our deployment"
-
